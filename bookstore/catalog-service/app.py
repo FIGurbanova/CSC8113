2e1fd306b22ae3dc962e98c599a67a9fb6ef0291
fiqurbanova <fi.qurbanova@gmail.com>
2025-03-16 02:36:21 +0000

    prometheus config in app.py

--- bookstore/catalog-service/app.py
+++ bookstore/catalog-service/app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from prometheus_client import generate_latest, CONTENT_TYPE_LATEST, Counter
 
+
 app = Flask(__name__)
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mysecretpassword@bookstore-db:5432/bookstore'
